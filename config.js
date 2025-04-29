1e127617de45732e217f29020843f9149534840e
Faton Klzs <fatonklzs@gmail.com>
2025-04-29 13:52:49 +0700

    First commit

---
+++ config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  BOT_TOKEN: "7713986992:AAGZ0fBsG0XuaPexzSXr4KvCamk5gihYMew",
+  OWNER_ID: ["5810676711"],
+};
+
+/*/━━━━━━━━━━━━━━━━━━━━━━  
+🔥 XVINCENT 🔥  
+━━━━━━━━━━━━━━━━━━━━━━  
+
+🚀 Developer  : RenXiter  
+📢 Info       : @RenIsDev  
+🛠️ Version    : 5.0  
+
+⚠️ **警告!** ⚠️  
+📌 此机器人使用数据库。  
+📌 如果您的令牌 **未在数据库中注册**，  
+📌 机器人将 **无法运行**！  
+📌 请确保您的令牌已注册后再使用本机器人。  
+
+⚠️ **PERINGATAN!** ⚠️  
+📌 Bot ini menggunakan database.  
+📌 Jika token Anda **tidak terdaftar di database**,  
+📌 bot **tidak akan berjalan**!  
+📌 Pastikan token Anda sudah terdaftar sebelum menggunakan bot ini.  
+
+━━━━━━━━━━━━━━━━━━━━━━/*/
